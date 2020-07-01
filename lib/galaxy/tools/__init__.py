08dcea27ae197f61ffb80bb8c9f4a1560bf196e0
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-07-02 00:12:47 +0530

    Force owncloud to be a regular tool

--- lib/galaxy/tools/__init__.py
+++ lib/galaxy/tools/__init__.py
@@ -1968,7 +1968,7 @@ class Tool(Dictifiable):
 
         tool_class = self.__class__
         # FIXME: the Tool class should declare directly, instead of ad hoc inspection
-        regular_form = tool_class == Tool or isinstance(self, (DatabaseOperationTool, InteractiveTool))
+        regular_form = tool_class == Tool or isinstance(self, (DatabaseOperationTool, InteractiveTool)) or self.id == "owncloud_import"
         tool_dict["form_style"] = "regular" if regular_form else "special"
 
         return tool_dict
