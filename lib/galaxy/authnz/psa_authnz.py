356a74341e6a1e65007c022c14f31241c67d3e9e
John Chilton <jmchilton@gmail.com>
2026-05-21 20:37:50 -0400

    Merge pull request #22484 from AustralianBioCommons/drs-access-token
    Allow attaching OIDC access token to DRS requests
    
    (cherry picked from commit f63ada64568ae853bcdb513184d79862013b9075)

--- lib/galaxy/authnz/psa_authnz.py
+++ lib/galaxy/authnz/psa_authnz.py
@@ -22,6 +22,7 @@ from sqlalchemy import func
 from sqlalchemy.exc import IntegrityError
 
 from galaxy import exceptions as galaxy_exceptions
+from galaxy.config import GalaxyAppConfiguration
 from galaxy.exceptions import MalformedContents
 from galaxy.managers import users as user_managers
 from galaxy.model import (
@@ -53,6 +54,19 @@ if TYPE_CHECKING:
     from galaxy.managers.context import ProvidesAppContext
 
 log = logging.getLogger(__name__)
+
+
+def locate_token_expiration(extra_data):
+    expires = extra_data.get("expires", None) or extra_data.get("expires_in", None)
+    if expires:
+        return expires
+
+    refresh_token = extra_data.get("refresh_token")
+    if refresh_token and isinstance(refresh_token, dict):
+        return refresh_token.get("expires", None) or refresh_token.get("expires_in", None)
+
+    return None
+
 
 # key: a component name which PSA requests.
 # value: is the name of a class associated with that key.
@@ -291,17 +305,7 @@ class PSAAuthnz(IdentityProvider):
         return False
 
     def _try_to_locate_refresh_token_expiration(self, extra_data):
-        # Try to get expiration from top-level keys
-        expires = extra_data.get("expires", None) or extra_data.get("expires_in", None)
-        if expires:
-            return expires
-
-        # Try to get expiration from refresh_token if it's a dict
-        refresh_token = extra_data.get("refresh_token")
-        if refresh_token and isinstance(refresh_token, dict):
-            return refresh_token.get("expires", None) or refresh_token.get("expires_in", None)
-
-        return None
+        return locate_token_expiration(extra_data)
 
     def authenticate(self, trans, idphint=None) -> "HttpResponseProtocol":
         on_the_fly_config(trans.sa_session)
